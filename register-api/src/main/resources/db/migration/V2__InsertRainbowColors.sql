de780648351a98395b4f1bf7c0a46a1fe409b854
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-22 11:44:20 -0300

    feature: inserção das cores básicas

---
+++ register-api/src/main/resources/db/migration/V2__InsertRainbowColors.sql
@@ -0,0 +1,20 @@
+INSERT INTO colors (name, hex_code) 
+    VALUES ('Vermelho', '#FF0000');
+
+INSERT INTO colors (name, hex_code) 
+    VALUES ('Laranja', '#FF7F00');
+
+INSERT INTO colors (name, hex_code) 
+    VALUES ('Amarelo', '#FFFF00');
+
+INSERT INTO colors (name, hex_code) 
+    VALUES ('Verde', '#00FF00');
+
+INSERT INTO colors (name, hex_code) 
+    VALUES ('Azul', '#0000FF');
+
+INSERT INTO colors (name, hex_code) 
+    VALUES ('Anil', '#4B0082');
+
+INSERT INTO colors (name, hex_code) 
+    VALUES ('Violeta', '#8B00FF');
