ec5dc51a01ede56fca98b9f1a9d6c12041715e0a
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-22 15:24:06 -0300

    feature: criação de rota para listagem das cores

--- register-api/src/main/java/carlosvinicios/colors/controller/ColorsController.java
+++ register-api/src/main/java/carlosvinicios/colors/controller/ColorsController.java
@@ -1,5 +1,7 @@
 package carlosvinicios.colors.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,11 @@ public class ColorsController {
 	@PostMapping()
 	public ResponseEntity<Color> createColor(@RequestBody() CreateColorDTO createColorDto){
 		return this.colorsService.createColor(createColorDto);
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<Color>> listColors(){
+		return this.colorsService.listColors();
 	}
 	
 	
