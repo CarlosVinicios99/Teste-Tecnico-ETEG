b3a6a09ecf84d8d8fed604eefd69bd7f8b0394f5
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-23 02:42:30 -0300

    feature: criação dos elementos do formulário

--- register-front-web/src/components/RegistrationForm.tsx
+++ register-front-web/src/components/RegistrationForm.tsx
@@ -1,9 +1,28 @@
-
+import "./RegistrationForm.css"
 
 const RegistrationForm = () => {
-  return (
-    <div></div>
-  )
+
+    return (
+        <div className="registration-form-container">
+            <h1 className="title">Cadastro De Usuário</h1>
+
+            <form className="form-content">
+                <input type="text" placeholder="Nome Completo"/>
+                <input type="text" placeholder="E-mail"/>
+                <input type="text" placeholder="CPF (somente números)"/>
+
+                <select className="select-option">
+                <option value="">Cor Preferida</option>
+                    <option value="Azul">Azul</option>
+                    <option value="Amarelo">Amarelo</option>
+                    <option value="Verde">Verde</option>
+                </select>
+                
+            </form>
+
+            <button className="registration-button">Cadastrar</button>
+        </div>
+    )
 }
 
 export default RegistrationForm
